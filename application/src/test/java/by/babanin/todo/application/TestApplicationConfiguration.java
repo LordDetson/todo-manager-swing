591ca0426bfa96fe3402c1d08ef239542e6f4f9d
Dmitry Babanin <dmitry.babanin.jazzteam@gmail.com>
2022-09-03 20:20:46 +0400

    Used AspectJ for transactions

--- application/src/test/java/by/babanin/todo/application/TestApplicationConfiguration.java
+++ application/src/test/java/by/babanin/todo/application/TestApplicationConfiguration.java
@@ -3,12 +3,15 @@ package by.babanin.todo.application;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @EnableJpaRepositories
+@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 @EntityScan("by.babanin.todo.model")
 @ComponentScan(basePackages = "by.babanin.todo.application")
 public class TestApplicationConfiguration {
